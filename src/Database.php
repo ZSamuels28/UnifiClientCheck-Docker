775afb67a2134f1dabc1d30f6fb45fc438531698
Rob Cork <robcork@gmail.com>
2024-05-26 13:04:49 -0700

    Ability to remove old devices that are no longer in unifi client list from known MAC database

--- src/Database.php
+++ src/Database.php
@@ -29,4 +29,16 @@ class Database {
         $stmt = $this->pdo->prepare("INSERT OR IGNORE INTO known_macs (mac_address) VALUES (?)");
         $stmt->execute([$mac]);
     }
+
+    public function removeOldMacs($clients) {
+        $result = $this->pdo->query("SELECT mac_address FROM known_macs");
+        foreach ($result as $row) {
+            $obj = array_column($clients, null, 'mac')[$row['mac_address']] ?? false;
+            if (!$obj){
+                echo "Removing device from database: " . ($row['name'] ?? "Unknown") . ", " . ($row['hostname'] ?? "N/A") . ", " . $row['mac_address'] . "\n";
+                $stmt = $this->pdo->prepare("DELETE FROM known_macs WHERE mac_address = :mac");
+                $stmt->execute(['mac' => $row['mac_address']]);
+            }
+        }
+    }
 }
